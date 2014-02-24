45b84d6bd55ff9a958647c6d0e7c7d8cc63a2404
Borna <borna@borna-almasi.com>
2014-02-23 20:15:55 -0500

    Added proper categories and data for menus and videos. Added red X and got it vs mark tough

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,58 +3,58 @@
 angular.module('batuApp')
   .controller('MainCtrl', function ($scope) {
     $scope.videoCollection = [
-        { link: "0KSOMA3QBU0", start: 30, watched: false },
-        { link: "hHimjVYsd6I", start: 20, watched: false },
-        { link: "kHue-HaXXzg", start: 10, watched: false }]
+      { name: "Rhythm", link: "t1DZfMfJ63M", start: 0, level: 0, group: "Samba Reggae"},
+      { name: "#1", link: "7rEyS90qpDk", start: 3, level: 0, group: "Samba Reggae" },
+      { name: "#2", link: "eCuYHYdLc_s", start: 3, level: 0, group: "Samba Reggae" },
+      { name: "#3", link: "IbC5kEUIgF4", start: 3.2, level: 0, group: "Samba Reggae" },
+      { name: "#4", link: "IvLXoWs00Rs", start: 3.3, level: 0, group: "Samba Reggae" },
+      { name: "#5", link: "nNRm_SVo86k", start: 3.3, level: 0, group: "Samba Reggae" },
+      { name: "#6", link: "weM6f7ctNXw", start: 1, level: 0, group: "Samba Reggae" },
+      { name: "#7", link: "2zzzRiy7L4Y", start: 3, level: 0, group: "Samba Reggae" },
+      { name: "#8", link: "ie_t_uJTibA", start: 3.3, level: 0, group: "Samba Reggae" },
+      { name: "Rhythm", link: "50n0xLkV9Mo", start: 0, level: 0, group: "Reggae" },
+      { name: "#1", link: "abJh99APBaA", start: 3, level: 0, group: "Reggae" },
+      { name: "#2", link: "qm-UvTyLN-E", start: 3, level: 0, group: "Reggae" },
+      { name: "Rhythm", link: "5d47OFYeb-g", start: 0, level: 0, group: "Rumba" },
+      { name: "#1", link: "9ncjTGtMe20", start: 3, level: 0, group: "Rumba" },
+      { name: "#2", link: "qRsHZaeIOi4", start: 3, level: 0, group: "Rumba" },
+      { name: "Rhythm", link: "_tVO56JlUxs", start: 0, level: 0, group: "Raggamuffin" },
+      { name: "#1", link: "rar-4aSxGYA", start: 3, level: 0, group: "Raggamuffin" },
+      { name: "#2", link: "VDZBlD8O6Ys", start: 3, level: 0, group: "Raggamuffin" },
+      { name: "#3", link: "RBWNcaVV_8M", start: 3, level: 0, group: "Raggamuffin" },
+      { name: "#4", link: "oAmaXFqRAIQ", start: 1, level: 0, group: "Raggamuffin" },
+      { name: "Rhythm", link: "FSJGTKDc114", start: 0, level: 0, group: "3:2" },
+      { name: "#1", link: "OxAVUPt-Wi8", start: 3, level: 0, group: "3:2" },
+      { name: "#2", link: "2WlFXCWJcWg", start: 2, level: 0, group: "3:2" },
+      { name: "#3", link: "O7_rwXjkLzA", start: 3, level: 0, group: "3:2" },
+      { name: "#4", link: "AjIq4s8m2WU", start: 3, level: 0, group: "3:2" },
+      { name: "#5", link: "FU2L-U9ky44", start: 3, level: 0, group: "3:2" },
+      { name: "#6", link: "IzSZO7JTqhE", start: 3, level: 0, group: "3:2" },
+      { name: "Rhythm", link: "OuuvDFOEBKU", start: 0, level: 0, group: "Timbalada" },
+      { name: "#1", link: "AVuHBHYFV_8", start: 3, level: 0, group: "Timbalada" },
+      { name: "#2", link: "NWF1521xMb4", start: 3, level: 0, group: "Timbalada" },
+      { name: "#1", link: "gEMcKbgZ0No", start: 0, level: 0, group: "Samba de Roda" }]
 
-    $scope.groupedCollection = _.groupBy($scope.videoCollection, 'watched');
+    $scope.groupedCollection = _.groupBy($scope.videoCollection, 'group');
 
-    $scope.$watch('videoCollection', function() {
-      $scope.groupedCollection = _.groupBy($scope.videoCollection, 'watched');
-    }, true);
+//    $scope.$watch('videoCollection', function() {
+//      $scope.groupedCollection = _.groupBy($scope.videoCollection, 'group');
+//    }, true);
 
-//      { link: "t1DZfMfJ63M", start: 0, watched: false },
-//      { link: "7rEyS90qpDk", start: 0, watched: false },
-//      { link: "eCuYHYdLc_s", start: 0, watched: false },
-//      { link: "IbC5kEUIgF4", start: 0, watched: false },
-//      { link: "IvLXoWs00Rs", start: 0, watched: false },
-//      { link: "nNRm_SVo86k", start: 0, watched: false },
-//      { link: "weM6f7ctNXw", start: 0, watched: false },
-//      { link: "2zzzRiy7L4Y", start: 0, watched: false },
-//      { link: "ie_t_uJTibA", start: 0, watched: false },
-//      { link: "50n0xLkV9Mo", start: 0, watched: false },
-//      { link: "abJh99APBaA", start: 0, watched: false },
-//      { link: "qm-UvTyLN-E", start: 0, watched: false },
-//      { link: "5d47OFYeb-g", start: 0, watched: false },
-//      { link: "9ncjTGtMe20", start: 0, watched: false },
-//      { link: "qRsHZaeIOi4", start: 0, watched: false },
-//      { link: "n7ogS7chMis", start: 0, watched: false },
-//      { link: "_tVO56JlUxs", start: 0, watched: false },
-//      { link: "rar-4aSxGYA", start: 0, watched: false },
-//      { link: "VDZBlD8O6Ys", start: 0, watched: false },
-//      { link: "RBWNcaVV_8M", start: 0, watched: false },
-//      { link: "oAmaXFqRAIQ", start: 0, watched: false },
-//      { link: "FSJGTKDc114", start: 0, watched: false },
-//      { link: "OxAVUPt-Wi8", start: 0, watched: false },
-//      { link: "2WlFXCWJcWg", start: 0, watched: false },
-//      { link: "O7_rwXjkLzA", start: 0, watched: false },
-//      { link: "AjIq4s8m2WU", start: 0, watched: false },
-//      { link: "FU2L-U9ky44", start: 0, watched: false },
-//      { link: "IzSZO7JTqhE", start: 0, watched: false },
-//      { link: "OuuvDFOEBKU", start: 0, watched: false },
-//      { link: "AVuHBHYFV_8", start: 0, watched: false },
-//      { link: "NWF1521xMb4", start: 0, watched: false },
-//      { link: "gEMcKbgZ0No", start: 0, watched: false }
+
 
   }).directive('youtube',function(){
     return {
       restrict: 'E',
       template:
-          '<div id="youtubeWrapper">' +
-            '<h3 id="timer" data-ng-hide="timer == 0">Starting video in {{timer}}s</h3>'+
-            '<div id="ytplayer"></div>' +
+        '<div id="youtubeWrapper">' +
+          '<h3 id="timer" data-ng-hide="timer == 0">Starting video in {{timer}}s</h3>'+
+          '<div id="ytplayer"></div>' +
           '</div>'+
-        '<p class="buttonSet"><a class="btn btn-lg btn-success" data-ng-click="nextVideo()">Next</a></p>',
+          '<p class="buttonSet">' +
+          '<a class="btn btn-lg btn-success" data-ng-click="currentVideo.level = 1;nextVideo()">Got it!</a>' +
+          '<a class="btn btn-lg btn-danger" data-ng-click="currentVideo.level = -1;nextVideo()">Mark as Tough</a>' +
+          '</p>',
       scope: {
         videos: "="
       },
@@ -74,7 +74,7 @@ angular.module('batuApp')
             } , 1000);
           };
 
-          $scope.player.seekTo(0.5).pauseVideo();
+          $scope.player.seekTo(0.1).pauseVideo();
           timerClick();
         }
 
@@ -93,18 +93,17 @@ angular.module('batuApp')
 
         $scope.getRandomVideo = function(){
           var numWatchedChecked = 0,
-              exhaustedList = false;
+            exhaustedList = false;
 
           this.currentVideo =  this.videos[Math.floor(Math.random()* this.videos.length)];
 
-          while ( this.currentVideo.watched == true && exhaustedList == false) {
+          while ( this.currentVideo.skill > 0 && exhaustedList == false) {
             this.currentVideo =  this.videos[Math.floor(Math.random()* this.videos.length)];
             numWatchedChecked++;
 
             if (numWatchedChecked !== this.videos.length) exhaustedList = true;
           }
 
-          this.currentVideo.watched = true;
           return this.currentVideo;
         };
 
